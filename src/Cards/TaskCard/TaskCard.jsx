dc61d03f0910487f8c29dbe7ef8f04baaf92e8cc
sakibsarkar <sakibsarkar707@gmail.com>
2024-02-06 18:38:59 +0600

    task edit functinality added

--- src/Cards/TaskCard/TaskCard.jsx
+++ src/Cards/TaskCard/TaskCard.jsx
@@ -1,15 +1,24 @@
 import "./TaskCard.css";
 import Swal from "sweetalert2";
+import { useEffect, useState } from "react";
 import { FaRegTrashCan } from "react-icons/fa6";
 import { MdKeyboardArrowDown, MdKeyboardArrowUp } from "react-icons/md";
 import { MdOutlineDone } from "react-icons/md";
+import { RxCross2 } from "react-icons/rx";
 import { toast } from "sonner";
 import { getItem } from "../../Hooks & Func/Localstorage";
 
-const TaskCard = ({ task, setShouldOpen, shouldOpen, index, setAllTask }) => {
+const TaskCard = ({ task, setShouldOpen, shouldOpen, index, setAllTask, }) => {
     const { taskName, taskDescription, taskDeadline, priority, status } = task || {}
 
     const isCompleted = status === "completed" ? true : false
+
+
+    // to get the new edited  prority 
+    const [selectedPriority, setSelectedPirity] = useState(priority)
+
+    // show the task edit form
+    const [showForm, setShowForm] = useState(false)
 
     const handleDeleteTask = () => {
 
@@ -58,6 +67,35 @@ const TaskCard = ({ task, setShouldOpen, shouldOpen, index, setAllTask }) => {
         toast.success("Hurra! you completed a task")
     }
 
+
+
+    const handleEditTask = (e) => {
+
+        e.preventDefault()
+
+        let allTask = getItem("tasks")
+
+        // edited values
+        const form = e.target
+        const new_taskName = form.name.value
+        const new_taskDescription = form.description.value
+        const new_taskDeadline = form.deadline.value || ""
+
+        // updating the values of the task 
+        allTask[index].taskName = new_taskName
+        allTask[index].taskDescription = new_taskDescription
+        allTask[index].taskDeadline = new_taskDeadline
+        allTask[index].priority = selectedPriority
+
+
+        // changing stats and localstorage
+        setAllTask(allTask)
+        localStorage.setItem("tasks", JSON.stringify(allTask))
+        toast.success("task updated")
+        setShowForm(false)
+
+    }
+
     return (
         <div className="box">
             <div className="title" onClick={() => setShouldOpen(index)}
@@ -85,11 +123,18 @@ const TaskCard = ({ task, setShouldOpen, shouldOpen, index, setAllTask }) => {
 
                 <div className="actions">
                     {
-                        isCompleted ? "" : <button
-                            style={{ background: "#64bf4f" }}
-                            onClick={handleComplete}
+                        isCompleted ? "" : <>
+                            <button
+                                style={{ background: "#64bf4f" }}
+                                onClick={handleComplete}
 
-                        ><MdOutlineDone />Completed</button>
+                            ><MdOutlineDone />Completed</button>
+
+                            <button
+                                style={{ background: "#3b7efd" }}
+                                onClick={() => setShowForm(true)}><FaRegTrashCan />Edit</button>
+
+                        </>
                     }
 
                     <button
@@ -100,6 +145,52 @@ const TaskCard = ({ task, setShouldOpen, shouldOpen, index, setAllTask }) => {
 
 
             </div>
+
+
+
+
+            {/* task edit form */}
+            {
+                showForm ?
+                    <div className="editForm">
+
+                        <form onSubmit={handleEditTask}>
+                            <div className="cross" onClick={() => setShowForm(false)}>
+                                <RxCross2 />
+                            </div>
+                            <div>
+                                <p>Task Title</p>
+                                <input type="text" name="name" required defaultValue={taskName} />
+                            </div>
+
+                            <div className="row">
+                                <div>
+                                    <p>Task priority</p>
+                                    <select onChange={(e) => setSelectedPirity(e.target.value)} value={selectedPriority}>
+                                        <option value="low">Low</option>
+                                        <option value="medium">Medium</option>
+                                        <option value="high">High</option>
+                                    </select>
+                                </div>
+
+                                <div>
+                                    <p>Task Deadline (optional)</p>
+                                    <input type="date" name="deadline" defaultValue={taskDeadline} />
+                                </div>
+                            </div>
+
+                            <div>
+                                <p>Task Description</p>
+                                <textarea name="description" required defaultValue={taskDescription} />
+                            </div>
+                            <button>Add new Task</button>
+                        </form>
+
+                    </div>
+
+                    :
+                    ""
+            }
 
 
         </div>
--- src/Pages/MyTask/MyTask.jsx
+++ src/Pages/MyTask/MyTask.jsx
@@ -2,14 +2,16 @@ import "./MyTask.css";
 import Notask from "../../Components/Notask/Notask";
 import TaskCard from "../../Cards/TaskCard/TaskCard";
 import { useState } from "react";
+import { FaTasks } from "react-icons/fa";
+import { VscTasklist } from "react-icons/vsc";
 import { getItem } from "../../Hooks & Func/Localstorage";
 
 const MyTask = () => {
 
     const [shouldOpen, setShouldOpen] = useState(0)
 
-    const [allTask, setAllTask] = useState(getItem("tasks"))
-
+    const [allTask = [], setAllTask] = useState(getItem("tasks"))
+    const completed = allTask?.filter(task => task.status === "completed")
 
     return (
         <div className="my_task_container">
@@ -19,21 +21,29 @@ const MyTask = () => {
 
                     <Notask state={"task"} />
                     :
+                    <>
+                        <div className="task_status">
+                            <h2 style={{ color: "#ff6f0d" }}><FaTasks />Total Task: {allTask?.length}</h2>
 
-                    <div className="task_boxs">
-                        {
-                            allTask?.map((task, index) => <TaskCard
-                                key={index}
-                                index={index}
-                                task={task}
-                                setAllTask={setAllTask}
-                                setShouldOpen={setShouldOpen}
-                                shouldOpen={shouldOpen} />
-                            )
-                        }
-                    </div>
+                            <h2 style={{ color: "#00c52a" }}><VscTasklist /> Completed task: {completed.length}</h2>
+                        </div>
 
 
+
+                        <div className="task_boxs">
+                            {
+                                allTask?.map((task, index) => <TaskCard
+                                    key={index}
+                                    index={index}
+                                    task={task}
+                                    setAllTask={setAllTask}
+                                    setShouldOpen={setShouldOpen}
+                                    shouldOpen={shouldOpen} />
+                                )
+                            }
+                        </div>
+
+                    </>
             }
         </div >
     );
